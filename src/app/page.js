39afe3de267dca7d43ffac0af2d1ab261d1c8f06
Kaung Phyo Wai <53686958+kaungphyowai@users.noreply.github.com>
2023-12-01 23:51:46 -0800

    user context solved

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,7 @@
-'use client'
+import HomePage from './HomePage'
 
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody, { tableBodyClasses } from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Stack from '@mui/material/Stack';
-import Paper from '@mui/material/Paper';
-import { Autocomplete, FormLabel, Radio, RadioGroup, Tab, Tabs } from '@mui/material';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { styled } from '@mui/material/styles';
-import CreateOrExtend from './UI/CreateOrExtend/CreateOrExtend'
-import ResponsiveAppBar from './UI/AppBar/AppBar'
-import ExtendUser from './UI/ExtendUser/ExtendUser'
-import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import config from '../amplifyconfiguration.json';
-Amplify.configure(config);
-
-
-export const UserContext = React.createContext()
-
-function HomePage({ signOut, user }) {
-  const [page, setPage] = React.useState(1);
-
-  return (
-    <UserContext.Provider value={user}>
-      <Container component='main' maxWidth='xl' disableGutters>
-        <ResponsiveAppBar setPage={setPage} signOut={signOut} />
-        <CssBaseline />
-        
-    <Container component='section' maxWidth='xs'>
-        {
-          page == 1 && <CreateOrExtend />
-        }
-        {
-          page == 2 && <ExtendUser />
-        }
-        
-    </Container>
-      </Container>
-      </UserContext.Provider>
-  );
+function App() {
+  return (<HomePage />)
 }
 
-export default withAuthenticator(HomePage);
+export default App;
