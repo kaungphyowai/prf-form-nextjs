bfe36738afe548491824273930970d354fd10fce
Kaung Phyo Wai <53686958+kaungphyowai@users.noreply.github.com>
2024-05-25 13:05:50 -0700

    testing setup

---
+++ src/app/utilites/pendingForm/admin/fetchPendingForm.js
@@ -0,0 +1,20 @@
+function fetchPendingForm()
+{
+    // query to the database
+    // 1. Status should be pending
+    // 2. 
+
+    // get the necessary data from the database
+
+
+    // 
+
+    return [
+        {
+            id: 1,
+            formSubmitter: "Mg Mg",
+            status: "Pending"
+        }
+    ];
+}
+module.exports = fetchPendingForm;
